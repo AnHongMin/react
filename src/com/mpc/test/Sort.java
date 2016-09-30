78ed7f160a34d718da408ef8d00adbd7039355be
hongmin.an <ahmuni02@naver.com>
2016-09-30 15:05:22 +0900

    list 정렬기능 자바스크립트 학습

---
+++ src/com/mpc/test/Sort.java
@@ -0,0 +1,32 @@
+package com.mpc.test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+
+public class Sort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ArrayList<String> list = new ArrayList<String>();
+		for(int i=0; i<10; i++){
+			list.add(String.valueOf(i));	
+		}
+		
+		Comparator<String> compare = new Comparator<String>(){			
+			public int compare(String o1, String o2) {
+				return o1.compareTo(o2);
+			}
+		};
+		// 오름차순
+		Collections.sort(list,compare);
+		
+		// 내림차순
+		Collections.reverse(list); 
+		for(int i=0; i<list.size(); i++){
+			System.out.println(list.get(i));
+		}
+	}
+}
